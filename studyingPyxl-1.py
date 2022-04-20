9cd56be0f631931ee197c47ec89aa1d5036fa070
naltosi-i <sol.naltosi@gmail.com>
2022-04-20 11:12:36 +0900

    small test

--- studyingPyxl-1.py
+++ studyingPyxl-1.py
@@ -1,10 +1,9 @@
-import imp
 import openpyxl
 import pathlib
 
-'''
 wb = openpyxl.load_workbook('studyingPyxlBook1.xlsx') # Excelファイルを読み込むメソッド
-wb.save('studyingPyxlBook2.xlsx') # 変数に格納されたExcelファイルを保存するメソッド
-'''
-
 pathlib.Path('test').mkdir(exist_ok=True)
+wb.save('test/studyingPyxlBook3.xlsx') # 変数に格納されたExcelファイルを保存するメソッド
+
+
+
